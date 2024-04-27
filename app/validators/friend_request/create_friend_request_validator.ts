6661deba77536546a8d28d639e74e5e35bc52700
Pierre <pbonnamy.pro@gmail.com>
2024-04-27 17:47:51 +0200

    create friend request

---
+++ app/validators/friend_request/create_friend_request_validator.ts
@@ -0,0 +1,25 @@
+import vine from '@vinejs/vine'
+import { AuthMetadata } from '../../types/auth_metadata.js'
+import User from '#models/user'
+
+export const createFriendRequestValidator = vine.withMetaData<AuthMetadata>().compile(
+  vine.object({
+    user_id: vine.number().exists(async (_, value, field) => {
+      if (value === field.meta.userId) {
+        return false
+      }
+
+      const user = await User.query()
+        .where('id', value)
+        .whereDoesntHave('sentFriendRequests', (query) => {
+          query.where('receiver_id', field.meta.userId)
+        })
+        .whereDoesntHave('receivedFriendRequests', (query) => {
+          query.where('sender_id', field.meta.userId)
+        })
+        .first()
+
+      return !!user
+    }),
+  })
+)
