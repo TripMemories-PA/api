57d6eef4c30c0f64ad33e967f9c4bcacd2610c5e
Pierre <pbonnamy.pro@gmail.com>
2024-06-15 22:39:05 +0200

    filter

--- app/types/requests/poi/index_poi_request.ts
+++ app/types/requests/poi/index_poi_request.ts
@@ -2,4 +2,8 @@ import { PaginateRequest } from '../paginate_request.js'
 
 export interface IndexPoiRequest extends PaginateRequest {
   search?: string
+  swLat?: number
+  swLng?: number
+  neLat?: number
+  neLng?: number
 }
--- app/validators/poi/index_poi_validator.ts
+++ app/validators/poi/index_poi_validator.ts
@@ -5,5 +5,9 @@ export const indexPoiValidator = vine.compile(
   vine.object({
     ...paginateRules,
     search: vine.string().maxLength(32).optional(),
+    swLat: vine.number().optional().requiredIfAnyExists(['swLng', 'neLat', 'neLng']),
+    swLng: vine.number().optional().requiredIfAnyExists(['swLat', 'neLat', 'neLng']),
+    neLat: vine.number().optional().requiredIfAnyExists(['swLat', 'swLng', 'neLng']),
+    neLng: vine.number().optional().requiredIfAnyExists(['swLat', 'swLng', 'neLat']),
   })
 )
